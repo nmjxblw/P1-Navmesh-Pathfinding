4923e0bcf01e0796ec5548255a503f20d2a09068
nmjxblw <zchen386@ucsc.edu>
2023-06-29 18:31:07 -0700

    Changes not staged for commit: 	modified:   README.md 	deleted:    input/homer.gif 	modified:   src/__pycache__/nm_pathfinder.cpython-39.pyc
    Untracked files:
    	.vscode/
    	input/test.png
    	input/test_image.png
    	input/test_image.png.mesh.pickle
    	input/test_image.png.mesh.png
    	src/__pycache__/nm_meshbuilder.cpython-39.pyc
    	src/to_bw.py

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -1,1 +1,37 @@
-def find_path (source_point, destination_point, mesh):    """    Searches for a path from source_point to destination_point through the mesh    Args:        source_point: starting point of the pathfinder        destination_point: the ultimate goal the pathfinder must reach        mesh: pathway constraints the path adheres to    Returns:        A path (list of points) from source_point to destination_point if exists        A list of boxes explored by the algorithm    """    path = []    boxes = {}    return path, boxes.keys()
+from math import inf, sqrt
+from heapq import heappop, heappush
+def find_path (source_point, destination_point, mesh):
+
+    """
+    Searches for a path from source_point to destination_point through the mesh
+
+    Args:
+        source_point: starting point of the pathfinder
+        destination_point: the ultimate goal the pathfinder must reach
+        mesh: pathway constraints the path adheres to
+
+    Returns:
+
+        A path (list of points) from source_point to destination_point if exists
+        A list of boxes explored by the algorithm
+    """
+
+    path = []
+    boxes = {}
+
+    path.push(source_point)
+    queue = []
+
+    heappush(queue,(0,source_point))
+
+    while queue:
+        break
+
+    return path, boxes.keys()
+
+def heuristic(a, b):
+     # Manhattan distance on a square grid
+   return abs(a.x - b.x) + abs(a.y - b.y)
+
+def calc_cost(box1,box2):
+    return 
